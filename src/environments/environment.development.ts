e0f1757d3ba4ff3b898397978207185c05efbdf0
Estefano Jaque Peña <u202225466@upc.edu.pe>
2024-10-20 11:41:47 -0500

    Update environment.development.ts
    Cambie a http://localhost:3000 para que el profesor pueda hacer la prueba del delete en el localhost

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {production: false,
                             // Server Base Path for Fake REST API
-                            serverBasePath: "https://my-json-server.typicode.com/estefanojaque/Managewise-reportes"
+                            serverBasePath: "http://localhost:3000"
                             // Server Base Path for Spring Boot REST API
                             //serverBasePath: 'http://localhost:8090/api/v1'
                             };
