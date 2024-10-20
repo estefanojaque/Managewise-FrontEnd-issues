be1caafe1b80b96775fb3b6af88a1027405ef355
Estefano Jaque Peña <u202225466@upc.edu.pe>
2024-10-20 11:42:21 -0500

    Update environment.ts
    cambie al localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {production: true,
-                            serverBasePath: "https://my-json-server.typicode.com/estefanojaque/Managewise-reportes"};
+                            serverBasePath: "http://localhost:3000"};
 
